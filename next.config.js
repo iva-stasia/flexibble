1f2fbbb86dc40b74d7c28bba4c699e1a04d270fe
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-10-11 00:16:47 +0300

    add upload project functionality

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "task.com"],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
+  },
 };
 
 module.exports = nextConfig;
